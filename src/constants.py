b68affd6dfe4a403de2493766d29046503705eef
Arturo Gonzalez <arturo@arturosolutions.com.au>
2019-05-30 14:35:54 +1000

    002 - Edited: Implemented file reading feature based on today's date.

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,5 @@
+from datetime import date
+
 url_us = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
 path_us = '../data/'
+file_name_us = "all_day-" + str(date.today().strftime('%d-%m-%Y')) + '.geojson'
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,18 @@
 import os
 
-from datetime import date
-from constants import path_us
+from constants import path_us, file_name_us
 
 
 class CreateDailyFile:
     root = os.path.dirname(__file__)
     path = path_us
-    file_name = "all_day-" + str(date.today().strftime('%d-%m-%Y')) + '.geojson'
+    file_name = file_name_us
     data = os.path.join(root, path, file_name)
 
 
 class ReadDailyFile:
     root = os.path.dirname(__file__)
     path = path_us
-    file_name = 'all_day-30-05-2019.geojson'
+    # file_name = 'all_day-30-05-2019.geojson'
+    file_name = file_name_us
     data = os.path.join(root, path, file_name)
