88458e5eab10ee2a57cabe562450494be3cc7ca5
Arturo Gonzalez <arturo@arturosolutions.com.au>
2019-05-30 10:47:49 +1000

    001 - Added: Comments.

--- src/main.py
+++ src/main.py
@@ -9,12 +9,18 @@ path = '../data/all_month.geojson'
 data = os.path.join(root, path)
 
 hc = HiveContext(sc)
+sc.setLogLevel("INFO")
 
 
 def get_place_and_magnitude():
+    """
+    :return: Magnitude and Places where magnitude is greater than 1.0
+    """
+    # Load JSON data
     json_data = hc.read.format("json").option("mode", "DROPMALFORMED").load(data)
+    # Create view earthquakes_us
     json_data.createOrReplaceTempView('earthquakes_us')
-    # json_data.write.format("orc").saveAsTable("earthquakes_us")
+    # Query magnitude and places where magnitude is greater than 1.0
     earthquakes_df = hc.sql("SELECT properties.mag, properties.place "
                             "FROM earthquakes_us "
                             "WHERE properties.mag > 1.0")
@@ -22,6 +28,9 @@ def get_place_and_magnitude():
 
 
 def main():
+    """
+    :return: Calling magnitude and places where magnitude is greater than 1.0 function
+    """
     get_place_and_magnitude()
 
 
